71d29abad195f583109a30801f479b9d9670d85b
jeffigy <evangelistajeffy25@gmail.com>
2022-03-23 11:34:16 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'uic-mobile-app-concept',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
